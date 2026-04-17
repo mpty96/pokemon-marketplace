a246468dc97d08ef593063fb36e245d95761ad36
mpty96 <sebainazuma@gmail.com>
2026-04-17 00:35:57 -0400

    fix: dark y light mode

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,8 +1,15 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
+
 const config: Config = {
   darkMode: 'class',
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: { extend: {} },
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
+
 export default config;
