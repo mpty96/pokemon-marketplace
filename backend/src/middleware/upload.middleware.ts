d0f6fcd86c5a78f8575d3b076d4b3371128bad40
mpty96 <sebainazuma@gmail.com>
2026-06-09 15:58:08 -0400

    fix tamaño de fotos en celulares

--- backend/src/middleware/upload.middleware.ts
+++ backend/src/middleware/upload.middleware.ts
@@ -18,5 +18,5 @@ const fileFilter = (
 export const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 3 * 1024 * 1024 }, // 3MB máximo
+  limits: { fileSize: 8 * 1024 * 1024 },
 });
